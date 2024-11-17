ffcd12b4a78287fd042a1529cd55c05a3cf53651
Thiago Gamarra <thiagogamarra82@gmail.com>
2024-11-17 15:14:48 -0800

    Create principal.py

---
+++ hortifruti_python/principal.py
@@ -0,0 +1,8 @@
+print("------Bem Vindo------")
+
+opcao = input(("digite entre 1-3"))
+def menu(opcao):
+    if opcao < 1 || opcao > 3:
+        print("digite novamente")
+    else:
+        print("parabens")
